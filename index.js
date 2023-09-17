a2502a49a20fc3f915a2bece4f9e6e614bc48211
Eren Çolak <erenncolak@hotmail.com>
2023-09-17 11:52:35 +0300

    Prototype

---
+++ index.js
@@ -0,0 +1,41 @@
+var pathCharacters = "./images/Characters/";
+function alyx() {
+    document.getElementById("imgCharacter").src = pathCharacters + "Alyx_Vance.png";
+}
+function gordon(){
+    document.getElementById("imgCharacter").src = pathCharacters + "Gordon_Freeman.jpg";
+}
+function eli() {
+    document.getElementById("imgCharacter").src = pathCharacters + "Eli_Vance.png";
+}
+function gman() {
+    document.getElementById("imgCharacter").src = pathCharacters + "G_man.png";
+}
+
+var pathPlaces = "./images/Places/";
+function novaProspekt() {
+    document.getElementById("imgPlaces").src = pathPlaces + "Nova_Prospekt.jpg";
+}
+function blackMesa() {
+    document.getElementById("imgPlaces").src = pathPlaces + "Black_Mesa.jpg";
+}
+function citadel() {
+    document.getElementById("imgPlaces").src = pathPlaces + "Citadel.jpg";
+}
+function ravenholm() {
+    document.getElementById("imgPlaces").src = pathPlaces + "Ravenholm.jpg";
+}
+
+var pathWeapons = "./images/Weapons/";
+function gravityGun() {
+    document.getElementById("imgWeapons").src = pathWeapons + "Gravity_Gun.jpg";
+}
+function crowbar() {
+    document.getElementById("imgWeapons").src = pathWeapons + "Crowbar.png";
+}
+function crossbow() {
+    document.getElementById("imgWeapons").src = pathWeapons + "Crossbow.jpg";
+}
+function shotgun() {
+    document.getElementById("imgWeapons").src = pathWeapons + "Shotgun.png";
+}
